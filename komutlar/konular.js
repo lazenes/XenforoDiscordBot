c9927b26a50b208a130c6c5bf185d463d9b6326e
Glitch (sinnercloeviribot) <none>
2021-04-04 19:09:30 +0000

    🏂🚓 Checkpoint ./komutlar/konular.js:30556008/1123

--- komutlar/konular.js
+++ komutlar/konular.js
@@ -15,7 +15,7 @@ if (!data.konular.length) {
   .setAuthor('Forum da son durum', message.author.displayAvatarURL())
   .setThumbnail(sonkonu["avatar"])
   .setColor('#ff1b1b')
-  .setDescription(`**Konu Adı:** \`${sonkonu["baslik"]}\` \n **Yayınlayan:** \`${sonkonu["konu_sahibi"]}\``)
+  .setDescription(`**Son Yorum:** \` ${sonkonu["sonYorum"]} \` \n **Yorum Sahibi:** \` ${sonkonu["yorumSahibi"]} \` \n **Konu Adı:** \` ${sonkonu["baslik"]} \` \n **Yayınlayan:** \` ${sonkonu["konuSahibi"]} \``)
   .addField("» Konu Linki", `[${sonkonu["baslik"]}](${sonkonu["KonuLink"]})`  , false)
   .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
  
