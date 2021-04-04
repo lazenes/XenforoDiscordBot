9781547985efb58a724edf8cddc926350ca309bb
Glitch (sinnercloeviribot) <none>
2021-04-04 17:36:27 +0000

    🎱🚞 Checkpoint ./komutlar/konular.js:30556008/740

--- komutlar/konular.js
+++ komutlar/konular.js
@@ -3,30 +3,28 @@ const fetch = require('node-fetch');
 const querystring = require('querystring');
 
 exports.run = async (client, message, args) => {
- var sonkonu = "";
-const body = await fetch("https://sinnerclownceviri.com/dcbot.php").then(response => {
-    data = response.json(); //data is a json object
-
+ 
+const data = await fetch("https://sinnerclownceviri.com/dcbot.php") .then(response => response.json());
+  
+if (!data.length) {
+            return message.channel.send(`Görüntülenecek İçerik Yok`);
+        }else{
  let konu = new Discord.MessageEmbed()
   .setAuthor('Forum Son Konu Bilgisi', message.author.displayAvatarURL())
   .setThumbnail(client.user.avatarURL())
   .setColor('#ff1b1b')
-  .setDescription(`**Konu Adı:** \`data['394']['baslik']\` \n **Yayınlayan:** \`data['394']['konu_sahibi']\``)
-  .addField("» KonuL inki", `[data['394']['baslik']](data['394']['KonuLink'])`  , false)
-
+  .setDescription(`**Konu Adı:** \`data.394.baslik\` \n **Yayınlayan:** \`data.394.konu_sahibi\``)
+  .addField("» Konu Linki", `[data.394.baslik](data.394.KonuLink)`  , false)
   .setFooter('Bu komutu kullanan kullanıcı ' + message.author.tag, message.author.displayAvatarURL())
-  sonkonu=konu
+ 
  
    return message.channel.send(konu);
   
-})
-.catch(e => {
-  return message.channel.send("JSON Verisine Erişilemiyor");
 }
-  )
+}
   
 
-      }
+      
 
 
 exports.conf = {
@@ -38,4 +36,4 @@ exports.help = {
   name: 'konular',
    description: 'Güncel Konuları Gösterir.',
   usage: 'konular'
-}
+};
